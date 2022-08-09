269388e277c54f36d2313b68803b5c49fc4d53b8
Lucas <lucasr4394@gmail.com>
2022-08-09 11:01:13 -0300

    alteração na seeder de reservas

--- almoxarifado/database/seeders/BookingSeeder.php
+++ almoxarifado/database/seeders/BookingSeeder.php
@@ -25,17 +25,5 @@ class BookingSeeder extends Seeder
 			'bookingDate' => '2021-07-28',
 			'transaction' => 'Reserva',
 		]);
-
-		\App\Models\Booking::factory()->create([
-			// 'id' => 2,
-			'user_id' => 1,
-			'user_name' => 'Lucas',
-			'equipment_type' => 'Notebook',
-			'equipment_id' => 2,
-			'patrimony' => 'Braip',
-			'quantity' => 1,
-			'bookingDate' => '2021-07-28',
-			'transaction' => 'Devolução',
-		]);
 	}
 }
